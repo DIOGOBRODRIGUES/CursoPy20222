69f3d7e36c61432bc06a2faacbfaa51850aad8e4
Diogo Rodrigues <diogo.borbar@gmail.com>
2022-08-01 21:48:17 -0300

    estrutura rep

--- curso1.py
+++ curso1.py
@@ -15,13 +15,27 @@
 #print('a soma dos dois números é ', soma)
 
 #ex4
-n1 = input('n1: ')
-n2 = input('n2: ')
-operacao = input('Digite a operação: ')
+##n1 = input('n1: ')
+##n2 = input('n2: ')
+##operacao = input('Digite a operação: ')
+##
+##resultado = None
+##
+##if operacao == '+':
+##    resultado = float(n1) + float(n2)
+##    print('{} + {} = {}'.format(n1, n2, resultado))
+##if operacao == '
 
-resultado = None
+#ex5
+##for c in range(1, 6):
+##    print('oi ', c)
+##
+##for c in range(1, 6, 2):
+##    print('oi ', c)
 
-if operacao == '+':
-    resultado = float(n1) + float(n2)
-    print('{} + {} = {}'.format(n1, n2, resultado))
-if operacao == '
+s = 0
+for c in range(0, 4):
+    n = int(input('Digite um valor: '))
+    s += n #s = s + n
+
+print('O somatório de todos os valores foi {}'.format(s))
