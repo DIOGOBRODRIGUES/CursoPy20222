dc37f115ba131c19ba04ac6776e89445b8a27f8c
Diogo Rodrigues <diogo.borbar@gmail.com>
2022-08-01 21:25:47 -0300

    curso Py

---
+++ curso1.py
@@ -0,0 +1,27 @@
+#ex1 
+#nome = input('Qual o seu nome?')
+#print('Seja bem vindo ' + nome )
+
+#ex2
+#dia = input ('Qual o dia?')
+#mes = input ('Qual o mês?')
+#ano = input ('Qual o ano de nascimento?')
+#print('Você nasceu no dia ' + dia + 'de ' + mes + 'de ' +ano ) 
+
+#ex3
+#num1 = int(input('Digite o primeiro número: '))
+#num2 = int(input('Outro número: '))
+#soma = num1 + num2
+#print('a soma dos dois números é ', soma)
+
+#ex4
+n1 = input('n1: ')
+n2 = input('n2: ')
+operacao = input('Digite a operação: ')
+
+resultado = None
+
+if operacao == '+':
+    resultado = float(n1) + float(n2)
+    print('{} + {} = {}'.format(n1, n2, resultado))
+if operacao == '
